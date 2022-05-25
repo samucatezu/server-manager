b25258b0c7984e2d7523d65918fa438791d53227
Samuel Almeida <barbosasamuel24@gmail.com>
2022-05-25 15:30:12 -0300

    first commit

---
+++ server-manager-backend/src/main/java/com/samucatezu/servermanagerbackend/model/ServerModel.java
@@ -0,0 +1,31 @@
+package com.samucatezu.servermanagerbackend.model;
+
+import com.samucatezu.servermanagerbackend.enumeration.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
+    private String ipAdress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
---
+++ server-manager-backend/src/test/java/com/samucatezu/servermanagerbackend/ServerManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samucatezu.servermanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
