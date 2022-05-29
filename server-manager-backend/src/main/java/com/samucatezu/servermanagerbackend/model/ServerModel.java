05c7d198bef9753e29cd1f98787296968223ec55
Samuel Almeida <barbosasamuel24@gmail.com>
2022-05-29 15:13:34 -0300

    HTTP resources added

--- server-manager-backend/src/main/java/com/samucatezu/servermanagerbackend/model/ServerModel.java
+++ server-manager-backend/src/main/java/com/samucatezu/servermanagerbackend/model/ServerModel.java
@@ -22,7 +22,7 @@ public class ServerModel {
     private Long id;
     @Column(unique = true)
     @NotEmpty(message = "IP Address cannot be empty or null")
-    private String ipAdress;
+    private String ipAddress;
     private String name;
     private String memory;
     private String type;
